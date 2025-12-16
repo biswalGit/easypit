a185a19de6f82939a990e0e899b753f69a0c5395
biswa <biswalhere@gmail.com>
2025-12-16 18:23:53 +0530

    docker compose chnages

--- Section2/cards/src/main/java/com/pit/cards/dto/CardsInfoDto.java
+++ Section2/cards/src/main/java/com/pit/cards/dto/CardsInfoDto.java
@@ -1,9 +1,17 @@
 package com.pit.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 @ConfigurationProperties(prefix = "card")
-public record CardsInfoDto(String cardNo, String type, Map<String,String> cardInfo, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsInfoDto{
+        String cardNo;
+        String type;
+        Map<String,String> cardInfo;
+        List<String> onCallSupport ;
 }
--- Section2/loans/src/main/java/com/pit/loans/dto/LoansInfoDto.java
+++ Section2/loans/src/main/java/com/pit/loans/dto/LoansInfoDto.java
@@ -1,11 +1,19 @@
 package com.pit.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loan")
-public record LoansInfoDto(String loanNo, String type, Map<String,String> loanInfo, List<String> onCallSupport) {
+@Getter
+@Setter
+public class LoansInfoDto{
+        String loanNo;
+        String type;
+        Map<String,String> loanInfo;
+        List<String> onCallSupport;
 }
 
